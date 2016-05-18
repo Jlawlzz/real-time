2683545c3d84d83e8b6df9b969b25cd89f33b5f0
Jordan Lawler <jordan.lawler@colorado.edu>
2016-05-17 18:15:50 -0600

    updated

--- test/survey-engine-test.js
+++ test/survey-engine-test.js
@@ -11,13 +11,13 @@ describe('survey engine functions', function(){
     let id = SurveyEngine.createSurveyId();
 
     assert.equal( id.length, 20 );
-  })
+  });
 
   it('creates admin id', function(){
     let id = SurveyEngine.createAdminId();
 
     assert.equal( id.length, 16 );
-  })
+  });
 
   it('parses questions and answers', function(){
     let body = { survey: { question: 'hello', options: ['yolo'] } }
@@ -25,7 +25,7 @@ describe('survey engine functions', function(){
 
     assert.equal( result.question, 'hello' );
     assert.equal( Object.keys(result.answers)[0], 'yolo' );
-  })
+  });
 
   it('parses answers to votes', function(){
     let options = ['yolo', 'noice', 'woopwoop'];
@@ -34,20 +34,20 @@ describe('survey engine functions', function(){
     assert.equal( result.yolo, 0 );
     assert.equal( result.woopwoop, 0 );
     assert.equal( result.noice, 0 );
-  })
+  });
 
   it('parses present time', function(){
     let options = { survey: { time: 1 } }
     let result = SurveyEngine.parseTime(options);
 
     assert.equal( result, 60000 );
-  })
+  });
 
   it('parses absent time', function(){
     let options = { survey: {question: 'hello'} }
     let result = SurveyEngine.parseTime(options);
 
     assert.equal( result, null );
-  })
+  });
 
 });
